e5b342f87a80c6d349b3091843b96c3b446d4616
do-bby <qkrdbstn25@naver.com>
2022-01-12 19:55:16 +0900

    algoritm

---
+++ CodeUp-basic/CodeUp-6083.py
@@ -0,0 +1,9 @@
+﻿r,g,b = input().split()
+r = int(r)
+b = int(b)
+g = int(g)
+for i in range(0,r):
+    for j in range(0,g):
+        for k in range(0,b):
+            print(i,j,k)
+print(r*g*b)
---
+++ CodeUp-basic/CodeUp-6085.py
@@ -0,0 +1,3 @@
+﻿w,h,b=input().split()
+res=int(w)*int(h)*int(b)/1024/1024/8
+print('%.2f'%res, "MB")
