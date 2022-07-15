8f0b98015cb5433ffda7abe203291bd61436b6cf
do-bby <qkrdbstn25@naver.com>
2022-07-15 16:55:39 +0900

    sudoku

---
+++ BaekJoon/단계별로 풀기/백준-백트래킹/백준 스도쿠.java
@@ -0,0 +1,75 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class Main{
+    public static int[][] arr = new int[9][9];
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        for(int i = 0; i <9; i++){
+            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+            for(int j = 0; j< 9; j++){
+                arr[i][j] = Integer.parseInt(st.nextToken());
+            }            
+        }
+        sudoku(0,0);
+    }
+    
+    public static void sudoku(int r, int c){
+        if(c == 9){
+            sudoku(r+1,0);
+            return;
+        }
+        if(r == 9){
+            StringBuilder sb = new StringBuilder();
+            for(int i = 0; i<9; i++){
+                for(int j = 0; j<9;j++){
+                    sb.append(arr[i][j]).append(" ");
+                }
+                sb.append('\n');
+            }
+            System.out.println(sb);
+            
+            System.exit(0);
+        }
+        
+        if(arr[r][c] == 0){
+            for(int i = 1; i<=9;i++){
+                if(possible(r,c,i)){
+                    arr[r][c] = i;
+                    sudoku(r,c+1);
+                }
+            }
+            arr[r][c] = 0;
+            return;
+        }
+        sudoku(r,c+1);
+    }
+    public static boolean possible(int r , int c, int value){
+        for(int i = 0; i<9;i++){
+            if(arr[r][i] == value){
+                return false;
+            }
+        }
+        
+        for(int i = 0; i<9;i++){
+            if(arr[i][c] == value){
+                return false;
+            }
+        }
+        
+        int srow = (r/3)*3;
+        int scol = (c/3)*3;
+        
+        for(int i = srow; i<srow+3;i++){
+            for(int j = scol; j<scol+3;j++){
+                if(arr[i][j] == value){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
